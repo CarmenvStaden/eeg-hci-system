4f074b4ffae828bdfe443475a47fd2569c5af03d
Adalys Garcia <adalys39@gmail.com>
2025-10-16 15:57:24 -0400

    comments for improved readability

--- backend-server/gamesession/views.py
+++ backend-server/gamesession/views.py
@@ -17,7 +17,7 @@ class EEGReadingCreateView(APIView):
     
 # Start a session    
 class SessionStartView(APIView):
-    def post(self, request):
+    def post(self, request): # incoming: Unity -> server
         patient = request.user
         game_id = request.data.get("game_id")
         prescription_id = request.data.get("prescription_id", None)
@@ -28,7 +28,8 @@ class SessionStartView(APIView):
             prescription_id=prescription_id,
             start_time=timezone.now()
         )
-        return Response(SessionSerializer(session).data, status=status.HTTP_201_CREATED)
+        serializer = SessionSerializer(session).data # deserialize: JSON -> django model instance
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 # End a session
 class SessionEndView(APIView):
@@ -41,14 +42,14 @@ class SessionEndView(APIView):
         except Session.DoesNotExist:
             return Response({"error": "Session not found"}, status=status.HTTP_404_NOT_FOUND)
         
-# Prescription Views: return list of prescriptions (get) or assign games through prescriptions (post)
+# Prescription Views
 class PrescriptionListCreateView(APIView):
     """
-    Only authenticated users (specialist) can see list of games. Only specialists can assign games.
+    Only authenticated users can see list of games (GET). Only specialists can assign games (POST).
     """
     permission_classes = [IsAuthenticated]
 
-    def get(self, request):
+    def get(self, request): # outgoing: server -> client
         if request.user.is_doctor:
             prescriptions = Prescription.objects.filter(doctor=request.user) # list prescriptions a doctor has assigned
         else:
@@ -56,11 +57,11 @@ class PrescriptionListCreateView(APIView):
         serializer = PrescriptionSerializer(prescriptions, many=True) # serialize the list to JSON for outgoing responses to client
         return Response(serializer.data)
 
-    def post(self, request):
+    def post(self, request): # incoming: client -> server
         if not request.user.is_doctor:
             return Response({"error": "Only specialists can assign games"}, status=status.HTTP_403_FORBIDDEN)
         
-        serializer = PrescriptionSerializer(data=request.data) # deserialize list from client to server
+        serializer = PrescriptionSerializer(data=request.data) # deserialize list from client (JSON) to server (model instance)
         if serializer.is_valid(): # if matches formatting in models
             serializer.save(doctor=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
