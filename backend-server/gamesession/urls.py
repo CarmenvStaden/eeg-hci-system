cfbbce0084e5a74b7a536a9139e95e4df14cae97
Adalys Garcia <adalys39@gmail.com>
2025-10-19 11:50:32 -0400

    api testing, modified serializers, added Games listcreat view, moved ordering of views to reflect proper ordering

--- backend-server/gamesession/urls.py
+++ backend-server/gamesession/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from gamesession.views import EEGReadingCreateView, SessionStartView, SessionEndView, PrescriptionListCreateView
+from gamesession.views import EEGReadingCreateView, SessionStartView, SessionEndView, PrescriptionListCreateView, GameListCreateView, SessionListCreateView
 
 # /gamesessions/...
 urlpatterns = [
-    path('eeg-readings/', EEGReadingCreateView.as_view(), name="eeg-reading-create"), # where Unity streams eeg-data
+    path('games/', GameListCreateView.as_view(), name='game-list-create'),
+    path("prescriptions/", PrescriptionListCreateView.as_view(), name="prescription-list-create"), # read/list (get) or create (post) prescriptions (house games)
+    path('sessions/', SessionListCreateView.as_view(), name='session-list-create'),
     path('sessions/start/', SessionStartView.as_view(), name='session-start'), # start a session
     path('sessions/<int:session_id>/end/', SessionEndView.as_view(), name='session-end'), # end a session
-    path("prescriptions/", PrescriptionListCreateView.as_view(), name="prescription-list-create"), # read/list (get) or create (post) prescriptions (house games)
+    path('eeg-readings/', EEGReadingCreateView.as_view(), name="eeg-reading-create"), # where Unity streams eeg-data
+
 ]
--- backend-server/gamesession/views.py
+++ backend-server/gamesession/views.py
@@ -1,15 +1,57 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Session, EEGReading, Prescription
-from .serializers import SessionSerializer, EEGReadingSerializer, PrescriptionSerializer
+from .models import Session, EEGReading, Prescription, Game
+from .serializers import SessionSerializer, EEGReadingSerializer, PrescriptionSerializer, GameSerializer
 from rest_framework.permissions import IsAuthenticated
 from django.utils import timezone
 
-# Create an EEG-Reading instance (unity POSTs here -> serializer -> model with all data -> DB)
-class EEGReadingCreateView(APIView):
-    def post(self, request): 
-        serializer = EEGReadingSerializer(data=request.data)
+class GameListCreateView(APIView): 
+    def get(self, request):
+        games = Game.objects.all()
+        serializer = GameSerializer(games, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = GameSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+# Prescription Views
+class PrescriptionListCreateView(APIView):
+    """
+    Only authenticated users can see list of games (GET). Only specialists can assign games (POST).
+    """
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request): # outgoing: server -> client
+        if request.user.is_doctor:
+            prescriptions = Prescription.objects.filter(doctor=request.user) # list prescriptions a doctor has assigned
+        else:
+            prescriptions = Prescription.objects.filter(patient=request.user) # list prescriptions a patient has been assigned
+        serializer = PrescriptionSerializer(prescriptions, many=True) # serialize the list to JSON for outgoing responses to client
+        return Response(serializer.data)
+
+    def post(self, request): # incoming: client -> server
+        if not request.user.is_doctor:
+            return Response({"error": "Only specialists can assign games"}, status=status.HTTP_403_FORBIDDEN)
+        
+        serializer = PrescriptionSerializer(data=request.data) # deserialize list from client (JSON) to server (model instance)
+        if serializer.is_valid(): # if matches formatting in models
+            serializer.save(doctor=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class SessionListCreateView(APIView):
+    def get(self, request):
+        sessions = Session.objects.all()
+        serializer = SessionSerializer(sessions, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = SessionSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -41,28 +83,17 @@ class SessionEndView(APIView):
             return Response(SessionSerializer(session).data)
         except Session.DoesNotExist:
             return Response({"error": "Session not found"}, status=status.HTTP_404_NOT_FOUND)
-        
-# Prescription Views
-class PrescriptionListCreateView(APIView):
-    """
-    Only authenticated users can see list of games (GET). Only specialists can assign games (POST).
-    """
-    permission_classes = [IsAuthenticated]
 
-    def get(self, request): # outgoing: server -> client
-        if request.user.is_doctor:
-            prescriptions = Prescription.objects.filter(doctor=request.user) # list prescriptions a doctor has assigned
-        else:
-            prescriptions = Prescription.objects.filter(patient=request.user) # list prescriptions a patient has been assigned
-        serializer = PrescriptionSerializer(prescriptions, many=True) # serialize the list to JSON for outgoing responses to client
+# Create an EEG-Reading instance (unity POSTs here -> serializer -> model with all data -> DB)
+class EEGReadingCreateView(APIView):
+    def get(self, request):
+        readings = EEGReading.objects.all()
+        serializer = EEGReadingSerializer(readings, many=True)
         return Response(serializer.data)
 
-    def post(self, request): # incoming: client -> server
-        if not request.user.is_doctor:
-            return Response({"error": "Only specialists can assign games"}, status=status.HTTP_403_FORBIDDEN)
-        
-        serializer = PrescriptionSerializer(data=request.data) # deserialize list from client (JSON) to server (model instance)
-        if serializer.is_valid(): # if matches formatting in models
-            serializer.save(doctor=request.user)
+    def post(self, request): 
+        serializer = EEGReadingSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
